3c0314098771754a8100907dc0c488cb4323111e
Glenn Brown <gbrown@bayareametro.gov>
2025-04-18 15:59:51 -0700

    feat: initial commit

--- lib/mtc-network-stack.ts
+++ lib/mtc-network-stack.ts
@@ -1,16 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { IpAddresses, Vpc } from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+export class MtcNetworkProps implements cdk.StackProps {
+  environment: String;
+  env: cdk.Environment;
+}
 export class MtcNetworkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: MtcNetworkProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MtcNetworkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new Vpc(this, `mtc-vpc-${props?.environment}`, {
+      ipAddresses: IpAddresses.cidr("10.0.0.0/16"),
+    });
   }
 }
